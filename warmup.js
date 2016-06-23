f02b288ded9bf5ae9ec9accd9ca1241905e26c59
Malachi Gray <malachigray@C02RC3QSG8WM.local>
2016-06-23 09:59:43 -0400

    complete warmup exercises

---
+++ warmup.js
@@ -0,0 +1,28 @@
+var myMax = function(array){
+	 var highNumber = null;
+	 array.forEach(function(el){
+		 if(el > highNumber){
+			 highNumber = el;
+		 }
+	 });
+	return highNumber;
+}
+
+console.log("myMax: " + myMax([1, 2, 6, 3, 9, 8, 4]));
+
+function vowelCount(string){
+  return (string.match(/[aeiou]/g)).length;
+}
+
+console.log("my vowel count " + vowelCount("hi my name is kitter"));
+
+function stringReverser(string){
+  var reversedString = "";
+  for (var i = (string.length - 1); i > -1; i--){
+    reversedString += string[i];
+  }
+  return reversedString;
+}
+
+console.log("my reversed string " + stringReverser("this is a string"));
+
