f5b0bdfa2c94b6644358fb0226e0a9e8a9a71112
Malachi Gray <malachigray@C02RC3QSG8WM.local>
2016-06-29 16:50:20 -0400

    display button press input in console

--- calculator.js
+++ calculator.js
@@ -13,3 +13,12 @@ function divide(numOne, numTwo){
 function subtract(numOne, numTwo){
   return numOne - numTwo;
 }
+
+var inputsArray = [];
+
+$(document).ready(function(){
+  $('.calcButton').on("click", function(){
+    console.log($(this).text());
+    runOperation($(this).text());
+  });
+});
