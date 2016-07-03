709c7d5c04d7d4e6f330c3107ea5f7c6c71d105e
Malachi Gray <graymd.cfi@gmail.com>
2016-07-03 13:52:23 -0400

    calculator working - need to display to screen

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ function add(numOne, numTwo){
   return numOne + numTwo;
 }
 
-function multiple(numOne, numTwo){
+function multiply(numOne, numTwo){
   return numOne * numTwo;
 }
 
@@ -15,10 +15,107 @@ function subtract(numOne, numTwo){
 }
 
 var inputsArray = [];
+var firstNumber = "";
+var operand = "";
+var secondNumber = "";
+var answer = "";
 
 $(document).ready(function(){
   $('.calcButton').on("click", function(){
-    console.log($(this).text());
-    runOperation($(this).text());
+    calculator($(this).text());
   });
 });
+
+var calculator = function calculator(userInput){
+  if (checkIsOperand(userInput)){
+      updateOperand(userInput);
+  } else if (checkIsClear(userInput)) {
+    clearInputs();
+  } else if (checkisEquals(userInput)){
+    console.log(calculate());
+  } else {
+    updateNumber(userInput);
+  }
+}
+
+var updateNumber = function updateNumber(userInput){
+  if (operand === ""){
+    updateFirstNumber(userInput);
+  } else {
+    updateSecondNumber(userInput);
+  }
+}
+
+var updateFirstNumber = function updateFirstNumber(userInput){
+  firstNumber += userInput;
+  console.log("this is firstNumber: " + firstNumber);
+}
+
+var updateSecondNumber = function updateSecondNumber(userInput){
+  secondNumber += userInput;
+  console.log("this is secondNumber: " + secondNumber);
+}
+
+var updateOperand = function updateOperand(userInput){
+  if (operand === ""){
+    operand = userInput;
+  } else {
+    calculate(userInput);
+  }
+}
+
+var checkIsOperand = function checkIsOperand(userInput){
+  return (userInput === "+") || (userInput === "-") || (userInput === "*") || (userInput === "/")
+}
+
+var calculate = function calculate(newOperand = ""){
+  if (operand === "+"){
+    answer = add(parseFloat(firstNumber), parseFloat(secondNumber));
+    resetInput(answer, newOperand);
+    return answer;
+  } else if (operand === "-") {
+    answer = subtract(parseFloat(firstNumber), parseFloat(secondNumber));
+    resetInput(answer, newOperand);
+    return answer;
+  } else if (operand === "*") {
+    answer = multiply(parseFloat(firstNumber), parseFloat(secondNumber));
+    resetInput(answer, newOperand);
+    return answer;
+  } else if (operand === "/"){
+    answer = divide(parseFloat(firstNumber), parseFloat(secondNumber));
+    resetInput(answer, newOperand);
+    return answer;
+  }
+}
+
+var checkisEquals = function checkisEquals(userInput){
+  return userInput === "=";
+}
+
+var resetInput = function resetInput(answer, newOperand){
+  firstNumber = answer;
+  resetOperand(newOperand);
+  clearSecondNumber();
+}
+
+var resetOperand = function resetOperand(newOperand = ""){
+  operand = newOperand;
+}
+
+var clearSecondNumber = function clearSecondNumber(){
+  secondNumber = '';
+}
+
+var clearFirstNumber = function clearFirstNumber(){
+  firstNumber = '';
+}
+
+var clearInputs = function clearInputs(){
+  clearFirstNumber();
+  clearSecondNumber();
+  resetOperand();
+}
+
+var checkIsClear = function checkIsClear(userInput){
+  return userInput === "CLEAR";
+}
---
+++ notes.md
@@ -0,0 +1,25 @@
+*** Creating calculator ***
+
+
+user inputs first number
+
+user inputs operand
+
+user inputs second number
+
+user hits enter
+
+
+if no operand then add to first number
+when operand is pressed add to operand
+after operand has been pressed add to second number
+when equals is pressed operation is executed
+
+
+
+------------------------------------------------
+
+when second operand is pressed equals is executed and new number is what
+is added to
+
+clear button clears everything
